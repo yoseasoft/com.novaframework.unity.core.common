aa7af19f87893f44ee59b11c2a4f1f20c16c2c92
hurley <yoseasoft@163.com>
2026-03-31 16:26:41 +0800

    新增预设配置文件；

---
+++ Runtime/Base/PresetConfiguration.cs
@@ -0,0 +1,48 @@
+/// -------------------------------------------------------------------------------
+/// Copyright (C) 2025 - 2026, Hainan Yuanyou Information Technology Co., Ltd. Guangzhou Branch
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+/// -------------------------------------------------------------------------------
+
+namespace NovaFramework
+{
+    /// <summary>
+    /// 通用模块定义的预设配置
+    /// </summary>
+    public static class PresetConfiguration
+    {
+        /// <summary>
+        /// 默认的原始资源文件夹名称
+        /// </summary>
+        public const string DefaultOriginalResourceFolderName = @"_Resources";
+        /// <summary>
+        /// 默认的原始资源文件夹相对路径
+        /// </summary>
+        public const string DefaultOriginalResourceRelativePath = @"Assets/_Resources";
+
+        /// <summary>
+        /// 项目默认“Editor”文件夹路径
+        /// </summary>
+        public const string DefaultProjectEditorRelativePath = @"Assets/Editor";
+        /// <summary>
+        /// 项目默认“Resources”文件夹路径
+        /// </summary>
+        public const string DefaultProjectResourceRelativePath = @"Assets/Resources";
+    }
+}
